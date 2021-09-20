fbd9d93ec55fcb4d2b90bcbde6f14eb6fa21e489
indhiradewanti <indhiradewantiputri@gmail.com>
2021-09-20 18:09:35 +0700

    migrating sequelize

---
+++ src/database/config.ts
@@ -0,0 +1,26 @@
+module.exports = {
+      development: {
+         dialect: 'postgres',
+         host: 'localhost',
+         port: 5432,
+         username: 'postgres',
+         password: 'postgres',
+         database: 'Testing',
+      },
+      test: {
+         dialect: 'postgres',
+         host: 'localhost',
+         port: 5432,
+         username: 'postgres',
+         password: 'postgres',
+         database: 'Testing',
+      },
+      production: {
+         dialect: 'postgres',
+         host: 'localhost',
+         port: 5432,
+         username: 'postgres',
+         password: 'postgres',
+         database: 'Testing',
+      },
+   }
---
+++ src/database/migrations/20210920105241-create-user.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        unique: true,
+        primaryKey: true,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
